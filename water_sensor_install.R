0e621c22d03f065e64a926e93caf05e71dce4136
brianwdavis <brianwdavis@gmail.com>
2021-04-19 23:52:10 -0400

    Move logic from two main versions into own functions

--- water_sensor_install.R
+++ water_sensor_install.R
@@ -20,6 +20,38 @@ parse_wsensor_install <- function(elt) {
   # "__version__" is the hashed version num
   # "_id" matches column in webhook submission (kobo table)
   
+  if (elt$`__version__` == "vuiiHRr2MJSGzFwSncyLP9") {
+    ret <- wsensor_install_1rep(elt)
+  } else {
+    ret <- wsensor_install_2rep(elt)
+  }
+  
+  if (nrow(ret) == 0) {stop("Some essential item is missing from the form")}
+  
+  return(ret)
+}
+
+wsensor_install_1rep <- function(elt) {
+  # timestamp is probably when they sync on Wifi (sent to kobo and webhooked)
+  # "Scan_your_bare_node" is bare node serial
+  # "What_is_your_Farm_Code" needs to have case coercion and whitespace trim
+  # "Scan_your_cover_crop_node" cover node serial
+  # "Scan_the_barcode_on_side_of_your_Gateway" gateway serial
+  # "start" is time of beginning of form fillout
+  # "end" is time of end of form fillout
+  # "In_which_subplot_are_ur_sensors_installed" trim to only last char for digit
+  # "What_is_the_GPS_loca_on_of_your_bare_node" c(lat, lon, alt_m, acc_m)
+  # "What_is_the_GPS_loca_your_cover_crop_node"
+  # "Any_issues_to_report_about_your_sensors" # should post these to github
+  # "_submitted_by" gives state acct so we can protect against code mistype?
+  # "What_day_did_you_install_the_sensors" seems to line up with other timestamps
+  # "_submission_time" can be some days after `end`
+  
+  # "_xform_id_string" is the ID for psa_water_sensor_install
+  # "_geolocation" is c(lat, lon) at time of opening? submitting?
+  # "__version__" is the hashed version num
+  # "_id" matches column in webhook submission (kobo table)
+  
   if (elt$`__version__` != "vuiiHRr2MJSGzFwSncyLP9") {
     stop("Incorrect form version, needs inspection")
   }
@@ -59,6 +91,14 @@ parse_wsensor_install <- function(elt) {
   
   return(ret)
 }
+
+
+
+wsensor_install_2rep <- function(elt) {
+  stop("Not finished yet")
+}
+
+
 
 etl_parse_wsensor_install <- function(elt) {
   
